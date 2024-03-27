30d92d995faf0a3b6f2bf52794460ae6660b7dc5
Juan Diego <134559247+JuanoneD@users.noreply.github.com>
2024-03-27 17:05:09 -0300

    Update Binary.h

--- Library/Binary.h
+++ Library/Binary.h
@@ -30,3 +30,36 @@
     }
 
 #endif
+#ifndef BANCO_H
+#define BANCO_H
+    float Deposit(float accountBalance,float depositValue){
+        FILE *arq = fopen("extrato.txt","a");
+        fprintf(arq,"Voce fez um deposito,seu saldo antigo é %.2f e depositou %.2f seu saldo atual é %.2f \n",accountBalance,depositValue,accountBalance + depositValue);
+        fclose(arq);
+        return accountBalance + depositValue;
+    }
+    float Withdraw(float accountBalance,float withdrawValue){
+        if(withdrawValue > accountBalance){
+            return accountBalance;
+        }
+        FILE *arq = fopen("extrato.txt","a");
+        fprintf(arq,"Voce fez um saque,seu saldo antigo é %.2f e sacou %.2f seu saldo atual é %.2f \n",accountBalance,withdrawValue,accountBalance - withdrawValue);
+        fclose(arq);
+        return accountBalance - withdrawValue;
+    }
+    float Lending(float accountBalance,float loanValue){
+        FILE *arq = fopen("extrato.txt","a");
+        fprintf(arq,"Voce fez um emprestimo,seu saldo antigo é %.2f e pediu  %.2f seu saldo atual é %.2f e sua divida é %.2f \n",accountBalance,loanValue,accountBalance + loanValue,loanValue);
+        fclose(arq);
+        return accountBalance + loanValue;
+    }
+    void extract(){
+        char line[100];
+        FILE *arq = fopen("extrato.txt","r");
+        while (fgets(line,sizeof(line),arq) != NULL){
+            printf("%s",line);
+        }
+        fclose(arq);
+    }
+
+#endif

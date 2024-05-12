21b9e3a05f65398495323f6a04b8688eb043d0df
JuanoneD <juandiego.jd42@gmail.com>
2024-05-12 20:20:48 -0300

    PORNO GAY

---
+++ 2024/05/12.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node Node;
+
+struct Node{
+    
+    Node * prev;
+    int data;
+
+};
+
+Node * create_node(int data){
+
+    Node * new_node = malloc(sizeof(Node));
+    new_node->data = data;
+    new_node->prev = NULL;
+
+    return new_node;
+
+};
+
+typedef struct Stack{
+
+    Node * top;
+    int size;
+
+}Stack;
+
+Stack construct_stack(){
+
+    Stack new_stack;
+
+    new_stack.top = NULL;
+    new_stack.size = 0;
+
+    return new_stack;
+
+};
+
+void push(Stack * stack,int data){
+
+    Node * new_node = create_node(data);
+
+    new_node->prev = stack->top;
+    stack->top = new_node;
+
+    stack->size ++;
+
+};
+
+int pop(Stack * stack){
+
+    Node * current = stack->top;
+
+    stack->size --;
+
+    int aux = current->data;
+
+    stack->top = current->prev;
+    free(current);
+
+    return aux;
+
+}
+
+int peek(Stack * stack){
+
+    return stack->top->data;
+
+}
+
+int getsize(Stack * stack){
+
+    return stack->size;
+
+}
+
+
+int main(){
+
+    Stack stack = construct_stack();
+
+    push(&stack,3);
+    push(&stack,2);
+    push(&stack,1);
+    push(&stack,4);
+    push(&stack,5);
+
+    printf(" \n %i ",pop(&stack));
+    printf(" \n %i ",peek(&stack));
+    printf(" \n %i ",getsize(&stack));
+
+
+
+
+    return 1;
+}
